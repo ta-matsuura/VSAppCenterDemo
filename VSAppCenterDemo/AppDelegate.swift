cc100901ae9cf1a3a60f537dbcada92fcc681bec
Taichi Matsuura <ta-matsuura@kddi-tech.com>
2018-01-10 16:25:15 +0900

    Added pod file.

--- VSAppCenterDemo/AppDelegate.swift
+++ VSAppCenterDemo/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +19,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        MSAppCenter.start("970d600d-a8e4-42e0-b53e-05b99ceaac4c", withServices:[
+            MSAnalytics.self,
+            MSCrashes.self
+            ])
         return true
     }
 
